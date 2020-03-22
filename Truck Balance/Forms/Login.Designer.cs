82d4442588f654f6025223d1e22809e0d27ed7d8
Mohamed Saad Eldin <Mohamed Saad Eldin@MS>
2020-03-22 19:49:01 +0200

    change image

--- Truck Balance/Forms/Login.Designer.cs
+++ Truck Balance/Forms/Login.Designer.cs
@@ -117,10 +117,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(311, 61);
+            this.pictureBox1.Location = new System.Drawing.Point(258, 70);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.Size = new System.Drawing.Size(300, 168);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
